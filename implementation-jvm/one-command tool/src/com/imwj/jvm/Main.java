ad5b3c33361e23731d097f7d2389822568a3871a
weixiaojian <2916863213@qq.com>
2022-10-08 14:22:48 +0800

    hello

---
+++ implementation-jvm/one-command tool/src/com/imwj/jvm/Main.java
@@ -0,0 +1,13 @@
+package com.imwj.jvm;
+
+/**
+ * @author wj
+ * @create 2022-10-08 14:22
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
