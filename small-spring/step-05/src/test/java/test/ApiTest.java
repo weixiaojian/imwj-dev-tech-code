acbc36a35179f25ee78816aa1f321bb123afed8d
weixiaojian <2916863213@qq.com>
2022-11-03 16:04:58 +0800

    为Bean对象注入属性和依赖Bean的功能实现

--- small-spring/step-05/src/test/java/test/ApiTest.java
+++ small-spring/step-05/src/test/java/test/ApiTest.java
@@ -50,7 +50,7 @@ public class ApiTest {
     @Test
     public void test_url() throws IOException {
         // 网络原因可能导致GitHub不能读取，可以放到自己的Gitee仓库。读取后可以从内容中搜索关键字；OLpj9823dZ
-        Resource resource = resourceLoader.getResource("https://github.com/fuzhengwei/small-spring/blob/main/important.properties");
+        Resource resource = resourceLoader.getResource("https://github.com/weixiaojian/study-code/blob/master/small-spring/step-05/src/main/resources/important.properties");
         InputStream inputStream = resource.getInputStream();
         String content = IoUtil.readUtf8(inputStream);
         System.out.println(content);
